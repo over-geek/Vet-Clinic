3034c0c7c1530eae0677f489e605318c0ccab0aa
kenagy100@gmail.com <kenagy100@gmail.com>
2023-08-18 13:28:56 +0000

    Create tables owners and species

--- schema.sql
+++ schema.sql
@@ -11,3 +11,22 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD COLUMN species VARCHAR;
+
+CREATE TABLE owners (
+    id bigserial primary key,
+    full_name varchar(255),
+    age int
+);
+
+CREATE TABLE species (
+    id bigserial primary key,
+    name varchar(255)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN species_id int;
+ALTER TABLE animals ADD CONSTRAINT foreign_key_species FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE animals ADD COLUMN owner_id int;
+ALTER TABLE animals ADD CONSTRAINT foreign_key_owners FOREIGN KEY (owner_id) REFERENCES owners(id);
