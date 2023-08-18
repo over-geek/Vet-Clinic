26dae7d4fda23f3fc04eaa64951ca893db75263c
kenagy100@gmail.com <kenagy100@gmail.com>
2023-08-18 11:55:38 +0000

    Insert data to animals table

--- data.sql
+++ data.sql
@@ -2,6 +2,13 @@
 
 INSERT INTO animals (id, name, date_of_birth, weight_kg, neutered, escape_attempts)
 VALUES ('Agumon', 'Feb 3, 2020', 10.23, true, 0),
-VALUES ('Gabumon', 'Nov 15, 2018', 8, true, 2),
-VALUES ('Pikachu', 'Jan 7, 2021', 15.04, false, 1),
-VALUES ('Devimon', 'May 12, 2017', 11, true, 5);
+('Gabumon', 'Nov 15, 2018', 8, true, 2),
+('Pikachu', 'Jan 7, 2021', 15.04, false, 1),
+('Devimon', 'May 12, 2017', 11, true, 5),
+('Charmander', '2020-02-08', -11.00, FALSE, 0),
+('Plantmon', '2021-11-15', -5.70, TRUE, 2),
+('Squirtle', '1993-04-02', -12.13, FALSE, 3),
+('Angemon', '2005-06-12', -45.00, TRUE, 0),
+('Boarmon', '2005-06-07', 20.40, TRUE, 7),
+('Blossom', '1998-09-13', 17.00, TRUE,3),
+('Ditto', '2022-05-14', 22.00, TRUE, 4);
