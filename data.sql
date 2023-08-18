ca5c06e6b38bfde2b1b58a1ea63fbedfb3d986db
kenagy100@gmail.com <kenagy100@gmail.com>
2023-08-18 03:44:18 +0000

    Insert data into animal table

--- data.sql
+++ data.sql
@@ -1,5 +1,7 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (id, name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES (1, 'Agumon', 'Feb 3, 2020', 10.23, true, 0),
+VALUES (2, 'Gabumon', 'Nov 15, 2018', 8, true, 2),
+VALUES (3, 'Pikachu', 'Jan 7, 2021', 15.04, false, 1),
+VALUES (4, 'Devimon', 'May 12, 2017', 11, true, 5);
