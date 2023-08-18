ed7c6eedeb9b399aa6387700481757e88946e9b2
kenagy100@gmail.com <kenagy100@gmail.com>
2023-08-18 10:31:21 +0000

    Add primary key to table

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id int,
-    name varchar(100),
+    id bigserial not null,
+    name varchar(100) not null,
     date_of_birth date,
-    escape_attempts int,
+    escape_attempts int not null,
     neutered boolean,
-    weight_kg decimal
+    weight_kg decimal not null,
+    primary key(id)
 );
