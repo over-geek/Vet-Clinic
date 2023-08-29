0781cc71ca8dc4f75c683e5c3a4a02ddab6e34a9
Henok <henokmekonnen5@gmail.com>
2023-08-30 01:46:58 +0300

    medical histories table create

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,7 +1,14 @@
 -- Patients table Create
 CREATE TABLE IF NOT EXISTS patients (
-    id INT GENERATED ALWAYS AS IDENTITY,
+    id serial PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    date_of_birth DATE,
-    PRIMARY KEY (id)
+    date_of_birth DATE
+);
+
+-- medical histories table creation
+CREATE TABLE IF NOT EXISTS medical_histories (
+    id serial PRIMARY KEY,
+    admitted_at TIMESTAMP NOT NULL,
+    patient_id INT REFERENCES patients(id),
+    status VARCHAR(255) NOT NULL
 );
