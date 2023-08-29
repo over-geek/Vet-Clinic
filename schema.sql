dbc3ce76898296de878b7766dde7757d84ca4392
Henok <henokmekonnen5@gmail.com>
2023-08-30 01:28:05 +0300

    index to animal id on visits table

--- schema.sql
+++ schema.sql
@@ -54,3 +54,6 @@ CREATE TABLE visits (
 
 -- Add an email column to your owners table
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+-- Add an index to the animal_id column of the visits table
+CREATE INDEX animal_id_idx ON visits (animal_id);
